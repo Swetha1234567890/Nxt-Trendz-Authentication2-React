146ffa5ee597113d00145b3d31e833f432c36fd3
mantri swetha <mantriswetha98@gmail.com>
2023-03-13 09:04:49 +0000

    Nxt part 2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      className="cart-img"
+      alt="cart"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Products = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      className="cart-img"
+      alt="products"
+    />
+  </div>
+)
+
+export default Products
